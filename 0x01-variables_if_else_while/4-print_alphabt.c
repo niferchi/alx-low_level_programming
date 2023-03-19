a141c2d45b61ae0197d3b47355aae456e3148595
nihad <ferchinihad@gmail.com>
2023-03-19 14:25:46 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
