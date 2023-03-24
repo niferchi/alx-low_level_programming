3488aa1e16ce26a12f4521dc11d01f92cb987622
nihad <ferchinihad@gmail.com>
2023-03-23 18:03:39 -0700

    task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-
-#endif /* MAIN_H */f
+int largest_number(int a, int b, int c);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+#endif /* MAIN_H */
